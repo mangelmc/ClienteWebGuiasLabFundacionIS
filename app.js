109c0d7c2207b96d70464a80f0410bfe1cab314b
Mike <mangelmcho@gmail.com>
2019-06-25 21:33:03 -0400

    a bit change at static folders

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const guiasRouter = require('./routes/guias');
 const homeRouter = require('./routes/home');
 var app = express();
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,9 +26,7 @@ app.set('port', process.env.PORT ||4000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(logger('dev'));
 
 
 app.use('/', indexRouter);
