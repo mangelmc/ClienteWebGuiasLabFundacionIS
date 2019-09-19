a3b7c8891785e5c72da19364d111d24a0f6b7717
Mike <mangelmcho@gmail.com>
2019-09-19 07:30:27 -0400

    graphs and bit changes added

--- public/javascripts/curso.js
+++ public/javascripts/curso.js
@@ -107,10 +107,11 @@ $(document).ready(()=>{
 				
 				for (let index = 0; index < data.length; index++) {
 					html = html+ t1 + data[index].numero + '</td><td class="desc">';
-					let contenido = data[index].contenidoHtml.split(/>/),texto = '';                            
+					/* let contenido = data[index].contenidoHtml.split(/>/),texto = '';                            
 					for(let i = 0; i < 6; i++){
 					texto += contenido[i].split(/</)[0] + ' ';
-					}
+                    } */
+                    let texto = 'Descripcion de guia'
 					html = html + texto + t2;
 					html = html + data[index].fechaRegistro.substr(0,10) + t2;  
 					html = html + a1 + data[index]._id + a2 + a3 + t3; 
@@ -132,4 +133,176 @@ $(document).ready(()=>{
 
          
     })
+    /* charts */
+    
+    let searchParams = new URLSearchParams(window.location.search);        
+			
+	let docente = searchParams.get('docente')
+    var idCurso = window.location.pathname.split( '/' )[2];
+    $.ajax({
+        type: "Get",
+        url: "http://localhost:8000/api/cursos/estadisticas?docente=" + docente + "&curso=" + idCurso,
+
+        cache: false,
+        dataType: "json",
+        //contentType: 'application/json; charset=utf-8',//multipart/form-data, or text/plain
+        headers:{
+            authorization: localStorage.getItem('authorization')||'bearer ',
+            
+        }
+    })            
+    .done(function(result) {
+        console.log( "success" ,result);
+
+        let guia = result.guias[0]
+        console.log('guia 0', guia.curso.gestion);
+        console.log(result.guias.length);
+
+
+        var labs = result.data;
+        var guias = result.guias;
+
+        var dcontPen = 0, dcontPor = 0,dcontRev = 0;
+        for (let i = 0; i < labs.length; i++) {
+           
+            if (labs[i].estado == 'revisado') {
+                dcontRev ++;
+            }else{
+                if (labs[i].estado == 'pendiente') {
+                    dcontPen ++;
+                }else{
+                    dcontPor ++;
+                }
+                
+            }
+            
+        }
+        var ctx = $('#pieChart');
+        //var ctx = 'myChart';
+        
+        var myChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+            labels: ['REVISADOS', 'NO REVISADOS', 'PENDIENTES'],
+            datasets: [{
+                label: 'Label',
+                data: [dcontRev , dcontPor, dcontPen],
+                backgroundColor: [
+                '#008d4c',
+                '#db8b0b',
+                '#d33724'
+                ],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            cutoutPercentage: 30,
+            responsive: true,
+            animation : {
+                animateScale:true,
+                animateRotate:true
+            }
+            
+        
+            }
+        });
+
+
+
+        var graf = [];
+        var rev=[],nrev=[],pen=[],crev=[],cnrev=[],cpen=[],llabs=[];
+        //for para los laboratoio
+        for (let i = 0; i < guias.length; i++) {
+            
+            const element = guias[i];
+            var contRev = 0 ,contPor = 0; contPen = 0;
+            for (let j = 0; j < labs.length; j++) {
+                if (labs[j].guia == guias[i]._id) {
+                    if (labs[j].estado == 'revisado') {
+                        contRev ++;
+                    }else{
+                        if (labs[j].estado == 'pendiente') {
+                            contPen ++;
+                        }else{
+                            contPor ++;
+                        }
+                        
+                    }
+                }
+            }
+            graf.push({
+                guia: guias[i]._id,
+                numero: guias[i].numero,
+                revisados: contRev,
+                nRevisados: contPor,
+                pendientes: contPen
+            })
+            rev.push(contRev);
+            nrev.push(contPor);
+            pen.push(contPen);
+            crev.push('#00a65a');
+            cnrev.push('#f39c12');
+            cpen.push('#f56954');
+            llabs.push('Lab '+guias[i].numero);
+           //aqui 
+        }
+
+        console.log('graf', graf);
+
+        var ctxBar = document.getElementById("barChart");
+        var myChartBar = new Chart(ctxBar, {
+            type: 'bar',
+            data: {
+            labels: llabs,
+            datasets: [{
+                label: 'REVISADOS',
+                data: rev,
+                backgroundColor: crev,
+                borderWidth: 1
+            },
+            {
+                label: 'NO REVISADOS',
+                data: nrev,
+                backgroundColor: cnrev,
+                borderWidth: 1
+            },
+            {
+                label: 'PENDIENTES',
+                data: pen,
+                backgroundColor: cpen,
+                borderWidth: 1
+            }]
+            },
+            options: {
+            responsive: false,
+            scales: {
+                
+                yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    
+                }
+                }]
+            }
+            }
+        });
+    
+        
+        
+        
+    })
+    .fail(function(err,status) {
+        console.log( "error" ,err.responseJSON.error.message);
+        
+        console.log( "status" ,status);
+    })
+    .always(function() {
+        console.log( "complete" );
+    });
+    
+    
+    
+    
+
+   
 })
